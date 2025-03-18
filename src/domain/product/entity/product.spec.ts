cf432d8e813e5ad8de75d30568b601da2174d1b4
chateaufischer <chateau.fischer@gmail.com>
2025-03-17 21:35:52 -0300

    Teste de Produto

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,33 +1,30 @@
 import Product from "./product";
 
 describe("Product unit tests", () => {
+
   it("should throw error when id is empty", () => {
     expect(() => {
       const product = new Product("", "Product 1", 100);
     }).toThrowError("Id is required");
   });
 
-  it("should throw error when name is empty", () => {
+  it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
+      const product = new Product("1", "", 100);
     }).toThrowError("Name is required");
   });
 
-  it("should throw error when price is less than zero", () => {
+  it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
+      const product = new Product("1", "Product", -10);
     }).toThrowError("Price must be greater than zero");
   });
 
-  it("should change name", () => {
-    const product = new Product("123", "Product 1", 100);
-    product.changeName("Product 2");
-    expect(product.name).toBe("Product 2");
+  it("should create a product", () => {
+    const product = new Product("1", "Product 1", 100);
+    expect(product.id).toBe("1");
+    expect(product.name).toBe("Product 1");
+    expect(product.price).toBe(100);
   });
 
-  it("should change price", () => {
-    const product = new Product("123", "Product 1", 100);
-    product.changePrice(150);
-    expect(product.price).toBe(150);
-  });
 });
